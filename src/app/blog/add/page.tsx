b2cfe13f4eb3e7a1ad68541620fd75e76e28c6e3
adf0423 <adf0423@gmail.com>
2023-08-27 00:15:19 +0900

    新規投稿処理の追加

--- src/app/blog/add/page.tsx
+++ src/app/blog/add/page.tsx
@@ -1,22 +1,43 @@
 "use client";
 
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { useRef } from "react";
+import { Toaster, toast } from 'react-hot-toast';
+
+const postBlog = async (title: string | undefined, description: string | undefined) => {
+  const res = await fetch(`http://localhost:3000/api/blog`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, description }),
+  });
+
+  return res.json();
+};
 
 const PostBlog = () => {
+  const router = useRouter();
 
   const titleRef = useRef<HTMLInputElement | null>(null);
   const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    console.log(titleRef.current?.value);
-    console.log(descriptionRef.current?.value);
+    toast.loading("投稿中です・・・", { id: "1"})
+    await postBlog(titleRef.current?.value, descriptionRef.current?.value)
+
+    toast.success("投稿に成功しました！", { id: "1"})
+
+    router.push("/");
+    router.refresh();
   }
 
   return (
     <>
+      <Toaster />
       <div className="w-full m-auto flex my-4">
         <div className="flex flex-col justify-center items-center m-auto">
           <p className="text-2xl text-slate-200 font-bold p-3">ブログ新規作成 🚀</p>
