46d2e55bc49c23207712229c811875e17124fe51
adf0423 <adf0423@gmail.com>
2023-08-26 23:23:44 +0900

    記事編集API作成

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -18,3 +18,23 @@ export const GET = async (req: Request, res: NextResponse) => {
     await prisma.$disconnect();
   }
 };
+
+//ブログ記事編集API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    const { title, description } = await req.json();
+
+    await main();
+    const post = await prisma.post.update( {
+      data: { title, description },
+      where: { id },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
